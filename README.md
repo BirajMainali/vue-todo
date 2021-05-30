3f647c998aafc2a4a87a2edcd88f3f2e3aa47fd4
Biraj Mainali <biraj.prathamit@gmail.com>
2021-05-30 23:40:51 +0545

    fix : Data persistence

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # TODO
+
 ![image](https://user-images.githubusercontent.com/71131016/120102589-7a277600-c16b-11eb-875b-7d519cc2a33b.png)
--- main.js
+++ main.js
@@ -1,59 +1,94 @@
 const IdGenerator = {
-  LastId: 0,
-  Generate() {
-    this.LastId += 1;
-    return this.LastId;
-  },
+    LastId: 0,
+    Generate() {
+        this.LastId += 1;
+        return this.LastId;
+    },
 };
 
 const app = Vue.createApp({
-  data() {
-    return {
-      allCompleted: false,
-      isCompleted: false,
-      todos: [],
-      todo: "",
-    };
-  },
-  methods: {
-    add() {
-      if (this.todo.trim().length === 0) return;
-      this.todos.push({
-        content: this.todo,
-        isCompleted: false,
-        id: IdGenerator.Generate(),
-      });
-      this.todo = "";
-      this.$refs["todoElm"].focus();
+    data() {
+        return {
+            editable: false,
+            editedContent: "",
+            allCompleted: false,
+            isCompleted: false,
+            todos: [],
+            todo: "",
+        };
     },
-    remove(id) {
-      const todo = this.todos.find((x) => x.id === id);
-      this.validator(todo);
-      this.todos = this.todos.filter((x) => x.id !== id);
-      this.$refs["todoElm"].focus();
+    methods: {
+        addTodo() {
+            if (this.todo.trim().length === 0) return;
+            this.todos.push({
+                id: IdGenerator.Generate(),
+                content: this.todo,
+                isCompleted: this.isCompleted,
+                isEditable: this.editable,
+            });
+            this.todo = "";
+            this.$refs["todoElm"].focus();
+        },
+        removeTodo(id) {
+            const todo = this.getTodoById(id);
+            this.validator(todo);
+            this.todos = this.todos.filter((x) => x.id !== id);
+            this.$refs["todoElm"].focus();
+        },
+
+        enableEdit(id) {
+            const todo = this.getTodoById(id);
+            this.validator(todo);
+            todo.isEditable = true;
+            this.$refs["editElm"].focus();
+        },
+
+        cancelEdit(id) {
+            this.getTodoById(id).isEditable = false;
+            this.$refs["todoElm"].focus();
+        },
+        saveChanges(id) {
+            const todo = this.getTodoById(id);
+            if (this.editedContent === "") {
+                this.editedContent = todo.content;
+            }
+            todo.content = this.editedContent;
+            todo.isEditable = false;
+            this.editedContent = "";
+            this.$refs["todoElm"].focus();
+        },
+        getTodoById(id) {
+            return this.todos.find((x) => x.id === id);
+        },
+        validator(todo) {
+            if (todo.isCompleted) {
+                const ask = confirm(
+                    `Are you sure This task has already been completed`
+                );
+            }
+        },
+        loadStoreItem() {
+            if (localStorage.todos) {
+                this.todos = JSON.parse(localStorage.todos);
+            }
+        },
     },
 
-    edit(id) {
-      const todo = this.todos.find((x) => x.id === id);
-      this.validator(todo);
-      this.todo = todo.content;
-      this.$refs["todoElm"].focus();
-      this.todos = this.todos.filter((x) => x.id !== id);
+    watch: {
+        allCompleted: function (val) {
+            for (let todo of this.todos) {
+                todo.isCompleted = val;
+            }
+        },
+        todos: {
+            handler(newTodos) {
+                localStorage.todos = JSON.stringify(newTodos);
+            },
+            deep: true,
+        },
     },
-    validator(todo) {
-      if (todo.isCompleted) {
-        const ask = confirm(
-          `Are you sure This task has already been completed`
-        );
-      }
-    },
-  },
 
-  watch: {
-    allCompleted: function (val) {
-      for (let todo of this.todos) {
-        todo.isCompleted = val;
-      }
+    mounted() {
+        this.loadStoreItem();
     },
-  },
 });
